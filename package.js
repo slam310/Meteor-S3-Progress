2e40488a5a717f13c8e6d450c3d202cc1ea80de5
digilord <dmorrigan@aeonstructure.com>
2013-12-27 12:59:04 -0700

    Added a path option to the S3 handlebars helper

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
-	summary: "Upload files to S3. Allows use of Knox Server-Side and get files urls on client-side."
+  summary: "Upload files to S3. Allows use of Knox Server-Side and get files urls on client-side."
 });
 
 Npm.depends({
-	knox: "0.8.5"
+  knox: "0.8.5"
 });
 
 Package.on_use(function (api) {
-	//Need service-configuration to use Meteor.method
-	api.use(["underscore", "ejson","service-configuration"], ["client", "server"]);
-	api.use(["handlebars", "spark"], "client");
-	api.add_files("s3client.js", "client");
-	api.add_files("s3server.js", "server");
+  //Need service-configuration to use Meteor.method
+  api.use(["underscore", "ejson","service-configuration"], ["client", "server"]);
+  api.use(["handlebars", "spark"], "client");
+  api.add_files("s3client.js", "client");
+  api.add_files("s3server.js", "server");
 
-	//Allows user access to Knox
-	api.export && api.export("Knox","server");
+  //Allows user access to Knox
+  api.export && api.export("Knox","server");
 });
--- s3server.js
+++ s3server.js
@@ -5,40 +5,50 @@ var knox;
 var S3;
 
 Meteor.methods({
-	S3config:function(obj){
-		knox = Knox.createClient(obj);
-		S3 = {directory:obj.directory || "/"};
-	},
-	S3upload:function(file,context,callback){
-		var future = new Future();
+  S3config:function(obj){
+    knox = Knox.createClient(obj);
+    S3 = {directory:obj.directory || "/"};
+  },
+  S3upload:function(options){
+    var file = options.file;
+    var context = options.context;
+    var callback = options.callback;
+    var path = options.path;
 
-		var extension = (file.name).match(/\.[0-9a-z]{1,5}$/i);
-		file.name = Meteor.uuid()+extension;
-		var path = S3.directory+file.name;
+    console.log('Calling S3upload');
+    var future = new Future();
 
-		var buffer = new Buffer(file.data);
+    var extension = (file.name).match(/\.[0-9a-z]{1,5}$/i);
+    file.name = Meteor.uuid()+extension;
+    if(path != null) {
+      path = path+"/"+file.name;
+    } else {
+      path = S3.directory+file.name;
+    }
 
-		knox.putBuffer(buffer,path,{"Content-Type":file.type,"Content-Length":buffer.length},function(e,r){
-			if(!e){
-				future.return(path);
-			} else {
-				console.log(e);
-			}
-		});
+    var buffer = new Buffer(file.data);
 
-		if(future.wait() && callback){
-			var url = knox.http(future.wait());
-			Meteor.call(callback,url,context);
-			return url;
-		}
-	},
-	S3delete:function(path, callback){
-		knox.deleteFile(path, function(e,r) {
-			if(e){
-				console.log(e);
-			}	else if(callback){
-				Meteor.call(callback);
-			}
-		});
-	}
+    knox.putBuffer(buffer,path,{"Content-Type":file.type,"Content-Length":buffer.length},function(e,r){
+      if(r){
+        future.return(path);
+      } else {
+        console.log(e);
+      }
+    });
+
+    var url = knox.http(future.wait());
+    if(url != null && typeof callback == 'string'){
+      console.log("URL: ", url);
+      Meteor.call(callback,url,context);
+    }
+  },
+  S3delete:function(path, callback){
+    knox.deleteFile(path, function(e,r) {
+      if(e){
+        console.log(e);
+      } else if(callback){
+        Meteor.call(callback);
+      }
+    });
+  }
 });
